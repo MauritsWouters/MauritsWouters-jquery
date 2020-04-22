75dfcd735f81d92110ab8b358eefafdefe080191
mauritsWouters <wouters.maurits@gmail.com>
2020-04-22 17:30:59 +0200

    Update script.js

--- PetervandenHeuvel-obelisk-jquery-1e56bf0caecb/05_getter-setter/script.js
+++ PetervandenHeuvel-obelisk-jquery-1e56bf0caecb/05_getter-setter/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
   // your code goes here
+
+  $("h1").css(function () {
+    var color = $(this).css("color");
+    console.log("That div is " + color + ".");
+  });
+
   // 01 - get color-value from h1
   // 02 - set color-value to h2
   // end of your code
