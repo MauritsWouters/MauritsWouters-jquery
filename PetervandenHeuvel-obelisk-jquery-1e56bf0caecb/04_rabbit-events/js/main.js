5fd08aa976cc716414240de31fae68c0d57e0ec2
mauritsWouters <wouters.maurits@gmail.com>
2020-04-22 17:30:56 +0200

    Update main.js

--- PetervandenHeuvel-obelisk-jquery-1e56bf0caecb/04_rabbit-events/js/main.js
+++ PetervandenHeuvel-obelisk-jquery-1e56bf0caecb/04_rabbit-events/js/main.js
@@ -1,5 +1,9 @@
-$(document).ready(function() {
-    // 1 Event that listens for click on img
-    // 2 Make the clicked image disappear with: $(this)...
-    // 3 Inside function:  Make clicked rabbit disapper, maybe use fadeOut() method, look on jquery.com: http://api.jquery.com/fadeout/
+$(document).ready(function () {
+  $("img").on("click", function () {
+    alert("Dit werkt");
+    $(this).fadeOut("slow");
+  });
+  // 1 Event that listens for click on img
+  // 2 Make the clicked image disappear with: $(this)...
+  // 3 Inside function:  Make clicked rabbit disapper, maybe use fadeOut() method, look on jquery.com: http://api.jquery.com/fadeout/
 }); // End of document.ready functie.
