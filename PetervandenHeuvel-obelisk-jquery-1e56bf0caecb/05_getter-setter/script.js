f46b01ff5a09dafa6f1c5e6f3d7c283647e8f08f
mauritsWouters <wouters.maurits@gmail.com>
2020-04-23 23:11:51 +0200

    Update script.js

--- PetervandenHeuvel-obelisk-jquery-1e56bf0caecb/05_getter-setter/script.js
+++ PetervandenHeuvel-obelisk-jquery-1e56bf0caecb/05_getter-setter/script.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
   // your code goes here
 
-  $("h1").val();
+  /*$("h1").val();
   $("h1").css(function () {
     var color = $(this).css("color");
     console.log("That div is " + color + ".");
-  });
-
+  });*/
+  /*
+  var colorValue = $("h1").val();
+  alert("color-value:" + colorValue);
+*/
+  $("h2").val("green");
   // 01 - get color-value from h1
   // 02 - set color-value to h2
   // end of your code
