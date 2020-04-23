a4a679fc0bcbe88778b5a9b44576c76ca0cfd4b6
mauritsWouters <wouters.maurits@gmail.com>
2020-04-23 22:48:35 +0200

    Update mainjs.js

--- challenge1/mainjs.js
+++ challenge1/mainjs.js
@@ -1,1 +1,16 @@
-$(":text").;
+$(document).ready(function () {
+  $("input").keyup(function () {
+    $("input").css("background-color", "pink");
+  });
+
+  $(".button").on("click", function () {
+    var inputValue = $("#inputtext").val().length;
+    alert(inputValue);
+  });
+
+  if (inputValue > 3) {
+  } else {
+    // $(".button").unbind("click");
+    $(".input").on("unfocus").text("te veel tekens");
+  }
+});
