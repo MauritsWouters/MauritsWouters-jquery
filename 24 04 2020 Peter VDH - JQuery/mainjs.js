38e8b3604f1d6eac763687cb1e48d9ed69df2bef
mauritsWouters <wouters.maurits@gmail.com>
2020-04-24 15:27:43 +0200

    Update mainjs.js

--- 24 04 2020 Peter VDH - JQuery/mainjs.js
+++ 24 04 2020 Peter VDH - JQuery/mainjs.js
@@ -5,15 +5,37 @@ $(document).ready(function () {
     var valueTicket = $("input:radio:checked").val();
     var prijsTickets = valueTicket * 11.5;
 
-    $(".foodDrinks").on("click", function () {
-      var valueFoodDrinks = $("input:checkbox:checked").val();
+    $('input[name="foodDrinks_name"]').on("click", function () {
+      let foodDrinks_name = [];
+      $.each($('input[name="foodDrinks_name"]:checked'), function () {
+        foodDrinks_name.push($(this).val());
 
-      $("#betalen").on("click", function () {
-        $("#item").text(prijsTickets);
-        $("#prijs").text(valueFoodDrinks);
+        selectedExtras = foodDrinks_name;
 
-        //alert("tickets" + prijsTickets);
-        //alert("foodDrinks" + valueFoodDrinks);
+        //$("input:checkbox:checked").val();
+
+        $("#betalen").on("click", function () {
+          var prijs = 0;
+          prijs = prijs + prijsTickets;
+
+          selectedExtras.forEach(function (foodDrinks_name, index) {
+            if (foodDrinks_name == "#popcorn") {
+              prijs = prijs + 5;
+            } else if (foodDrinks_name == "#cola") {
+              prijs = prijs + 2.5;
+            }
+          });
+
+          var totaal = parseFloat(prijsTickets) + parseFloat(foodDrinks_name);
+          $("#prijsTickets").text("Tickets: " + prijsTickets + " euro");
+          $("#prijsFoodDrinks").text(
+            "Food & Drinks: " + foodDrinks_name + " euro"
+          );
+          $("#prijsTotaal").text("Food & Drinks: " + totaal + " euro");
+
+          //alert("tickets" + prijsTickets);
+          //alert("foodDrinks" + valueFoodDrinks);
+        });
       });
     });
   });
