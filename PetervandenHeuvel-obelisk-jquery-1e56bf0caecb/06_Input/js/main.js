7c176708025db989094de47161b752f79ac207ea
mauritsWouters <wouters.maurits@gmail.com>
2020-04-24 10:37:26 +0200

    Update main.js

--- PetervandenHeuvel-obelisk-jquery-1e56bf0caecb/06_Input/js/main.js
+++ PetervandenHeuvel-obelisk-jquery-1e56bf0caecb/06_Input/js/main.js
@@ -1,4 +1,29 @@
-$(document).ready(function() {
-        // Your Code here
-        
+$(document).ready(function () {
+  // Your Code here
+
+  $("#btn-hello").on("click", function () {
+    alert("Hello");
+  });
+
+  /*$("input:submit").on("click", function () {
+    if ($("input:submit").val(" ")) {
+      alert("Empty");
+    }
+  });*/
+
+  $("input:submit").click(function () {
+    if ($("#firstname").val() == "") {
+      $("#firstname").css("border", "5px solid red");
+      alert("niet submitten");
+    } else if ($("#lastname").val() == "") {
+      $("#lastname").css("border", "5px solid red");
+      alert("niet submitten");
+    } else {
+      alert("bedankt voor het invullen");
+    }
+  });
+
+  /*$( "#target" ).click(function() {
+        $('#editor-area').val('');
+      });*/
 }); // End of document.ready functie.
